a2a18c7162e79d9a3974a479de2e851a1770afe6
Felix Aristio <felixaristio@gmail.com>
2023-06-15 17:54:33 +0700

    update js

---
+++ main.js
@@ -0,0 +1,10 @@
+var button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+  alert("Tombol telah diklik!");
+});
+
+
+function submitForm() {
+    alert("Pesan anda telah terkirim!");
+}
